f820c956f751c959eadefc48f8eb81231c6d2d61
unknown <qarshiyevnavruz7777@gmail.com git config --global user.email qarshiyevnavruz7777@gmail.com>
2023-01-09 10:49:27 +0500

    b

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 # Get the token from the environment variables
 TOKEN = os.environ['TOKEN']
 # Create a bot object
-bot = Bot(TOKEN)
+bot = Bot('5503823244:AAFNZ-echSZpypl7dIKeeMLroXN7OBraNHo')
 # Print the bot info
 print(bot.getMe())
 
 # Send a message to the bot
-bot.sendMessage(123456789,"Hello World")
+bot.sendMessage(123456789,"Hello World") 
