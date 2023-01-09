40d71ce77b2fe98667d71b8847403d8aaa28c3fc
ZARIFMAC <uzbzarif@gmail.com>
2023-01-09 09:44:19 +0500

    print bot info

---
+++ main.py
@@ -0,0 +1,9 @@
+from telegram.bot import Bot
+import os 
+
+# Get the token from the environment variables
+TOKEN = os.environ['TOKEN']
+# Create a bot object
+bot = Bot(TOKEN)
+# Print the bot info
+print(bot.getMe())
